f9d16c1330332c097c6eeb0156a37945da9547e1
Yrol <yrollgayanth@yahoo.com>
2022-10-20 22:53:23 +1100

    Adding POST, UPDATE and DELETE request methods

--- src/main/java/com/yrol/restplayground_1/controllers/StudentController.java
+++ src/main/java/com/yrol/restplayground_1/controllers/StudentController.java
@@ -2,15 +2,14 @@ package com.yrol.restplayground_1.controllers;
 
 
 import com.yrol.restplayground_1.entitites.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+//@RequestMapping("students")
 public class StudentController {
 
     /**
@@ -28,7 +27,7 @@ public class StudentController {
      * http://localhost:8080/student/2
      * */
     @GetMapping("student/{id}")
-    public Student getStudent(@PathVariable int id) {
+    public Student getStudent(@PathVariable("id") int id) {
         Student student = new Student(id, "John", "Cena");
         return student;
     }
@@ -54,5 +53,35 @@ public class StudentController {
     @GetMapping("student/unique")
     public Student getUniqueStudent(@RequestParam int id, @RequestParam String firstname, @RequestParam String lastname) {
         return new Student(id, firstname, lastname);
+    }
+
+    /**
+     * PostMapping and RequestBody
+     * Use Postman to post data {firstName:"Elon", lastName:"Musk"}
+     * http://localhost:8080/student/new
+     * */
+    @PostMapping(path = "student/new", consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student newStudent(@RequestBody Student student) {
+        return new Student(1, student.getFirstName(), student.getLastName());
+    }
+
+    /**
+     * PatchMapping(partial update) with PathVariable and RequestBody
+     * Use Postman to patch data {firstName:"Elon", lastName:"Musk"}
+     * http://localhost:8080/student/update/23
+     * */
+    @PatchMapping(path = "student/update/{id}", consumes = "application/json")
+    public Student updateStudent(@PathVariable("id") int id,  @RequestBody Student patchStudent) {
+        Student student = new Student(id, "Van", "Damme");
+        student.setFirstName(patchStudent.getFirstName());
+        student.setLastName(patchStudent.getLastName());
+        return student;
+    }
+
+    @DeleteMapping("student/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteStudent(@PathVariable("id") int id) {
+
     }
 }
