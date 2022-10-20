4aab29ff6561231276ff1b4f2a16c1e5170e6798
Yrol <yrollgayanth@yahoo.com>
2022-10-20 20:51:34 +1100

    initial commit

---
+++ src/main/java/com/yrol/restplayground_1/RestPlayground1Application.java
@@ -0,0 +1,13 @@
+package com.yrol.restplayground_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestPlayground1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestPlayground1Application.class, args);
+	}
+
+}
