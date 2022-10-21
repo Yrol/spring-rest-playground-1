ce062b77efc7d86be8b036a32cd604bee4b12b42
Yrol <yrollgayanth@yahoo.com>
2022-10-21 21:11:01 +1100

    Applying @RequestMapping to the controller

--- src/main/java/com/yrol/restplayground_1/controllers/StudentController.java
+++ src/main/java/com/yrol/restplayground_1/controllers/StudentController.java
@@ -3,30 +3,46 @@ package com.yrol.restplayground_1.controllers;
 
 import com.yrol.restplayground_1.entitites.Student;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-//@RequestMapping("students")
+@RequestMapping("/student")
 public class StudentController {
 
     /**
-     * GetMapping
-     * http://localhost:8080/student/
+     * GetMapping with ResponseEntity
+     * http://localhost:8080/student/student
      * */
-    @GetMapping("student")
-    public Student getStudent() {
+    @GetMapping("/student")
+    public ResponseEntity<Student> getStudent() {
         Student student = new Student(1, "James", "Webb");
-        return student;
+
+        // ok() by default return 200
+        return ResponseEntity.ok(student);
+    }
+
+    /**
+     * GetMapping with ResponseEntity and custom value in the response header
+     * http://localhost:8080/student/1/token
+     * */
+    @GetMapping("/{id}/token")
+    public ResponseEntity<Student> getStudentWithToken(@PathVariable("id") int id) {
+        Student student =  new Student(id, "Aaron", "Silva");
+
+        return ResponseEntity.ok()
+                .header("token", "abc12356")
+                .body(student);
     }
 
     /**
      * GetMapping with PathVariable
      * http://localhost:8080/student/2
      * */
-    @GetMapping("student/{id}")
+    @GetMapping("/{id}")
     public Student getStudent(@PathVariable("id") int id) {
         Student student = new Student(id, "John", "Cena");
         return student;
@@ -34,9 +50,9 @@ public class StudentController {
 
     /**
      * GetMapping returns a list of students
-     * http://localhost:8080/students
+     * http://localhost:8080/student/all
      * */
-    @GetMapping("students")
+    @GetMapping("/all")
     public List<Student> getStudents() {
         List<Student> students = new ArrayList<>();
         students.add(new Student(1, "John", "Cena"));
@@ -50,7 +66,7 @@ public class StudentController {
      * GetMapping with RequestParams
      * http://localhost:8080/student/unique?id=1&firstname=James&lastname=Bond
      * */
-    @GetMapping("student/unique")
+    @GetMapping("/unique")
     public Student getUniqueStudent(@RequestParam int id, @RequestParam String firstname, @RequestParam String lastname) {
         return new Student(id, firstname, lastname);
     }
@@ -60,10 +76,13 @@ public class StudentController {
      * Use Postman to post data {firstName:"Elon", lastName:"Musk"}
      * http://localhost:8080/student/new
      * */
-    @PostMapping(path = "student/new", consumes = "application/json")
+    @PostMapping(path = "/new", consumes = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
-    public Student newStudent(@RequestBody Student student) {
-        return new Student(1, student.getFirstName(), student.getLastName());
+    public ResponseEntity<Student> newStudent(@RequestBody Student newStudent) {
+
+        Student student = new Student(1, newStudent.getFirstName(), newStudent.getLastName());
+
+        return new ResponseEntity<>(student, HttpStatus.CREATED);
     }
 
     /**
@@ -71,7 +90,7 @@ public class StudentController {
      * Use Postman to patch data {firstName:"Elon", lastName:"Musk"}
      * http://localhost:8080/student/update/23
      * */
-    @PatchMapping(path = "student/update/{id}", consumes = "application/json")
+    @PatchMapping(path = "/update/{id}", consumes = "application/json")
     public Student updateStudent(@PathVariable("id") int id,  @RequestBody Student patchStudent) {
         Student student = new Student(id, "Van", "Damme");
         student.setFirstName(patchStudent.getFirstName());
@@ -79,7 +98,11 @@ public class StudentController {
         return student;
     }
 
-    @DeleteMapping("student/{id}")
+
+    /**
+     * http://localhost:8080/student/23
+     * */
+    @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteStudent(@PathVariable("id") int id) {
 
